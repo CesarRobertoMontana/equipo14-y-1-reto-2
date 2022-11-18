44592ca4fb77b11f9dfb849eb957a5e77b1d86db
CesarRobertoMontana <crmontana@uninorte.edu.co>
2022-11-17 20:44:07 -0500

    equipo 14 y 1 cambios

---
+++ lib/ui/controllers/shopping_controller.dart
@@ -0,0 +1,63 @@
+import 'package:get/get.dart';
+import 'package:loggy/loggy.dart';
+
+import '../../domain/product.dart';
+
+class ShoppingController extends GetxController {
+  // lista de productos
+  var entries = <Product>[].obs;
+  // el valor total de la compra
+  var total = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    // los dos elementos que vamos a tener en la tienda
+    entries.add(Product(0, "Coches de Juguete", 10));
+    entries.add(Product(1, "Casas de Juguete", 20));
+  }
+
+  void calcularTotal() {
+    int newTotal = 0;
+    // TODO
+    // calcular el valor total de los elementos en el carro de compras
+
+    for (var i = 0; i < entries.length; i++) {
+      Product producto = entries[i];
+      newTotal = producto.quantity * producto.price;
+    }
+
+    total.value = newTotal;
+  }
+
+  agregarProducto(id) {
+    logInfo('agregarProducto $id');
+    // TODO
+    // Encontrar el elemento usando el id, revisar el método firstWhere de la lista
+    // después obtener el index de ese elemento, revisar el método indexOf de la lista
+    // después hacer el incremento en la cantidad
+    // finalmente actualizar entries usando el indice y el elemento actualizado
+    Product producto = entries.firstWhere((element) => id == element.id);
+    int posicion = entries.indexOf(producto);
+    producto.quantity = producto.quantity + 1;
+    entries[posicion] = producto;
+
+    calcularTotal();
+  }
+
+  quitarProducto(id) {
+    logInfo('quitarProducto $id');
+    // TODO
+    // similar a agregarProducto
+    // validar cuando la cantidad es igual a cero
+    Product producto = entries.firstWhere((element) => id == element.id);
+    int posicion = entries.indexOf(producto);
+
+    if (producto.quantity > 0) {
+      producto.quantity = producto.quantity - 1;
+    }
+    entries[posicion] = producto;
+
+    calcularTotal();
+  }
+}
---
+++ lib/ui/widgets/banner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomBanner extends StatelessWidget {
+  const CustomBanner(this.bannerHeight, {Key? key}) : super(key: key);
+  final double bannerHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: bannerHeight,
+      child: Image.network(
+          "https://images.unsplash.com/photo-1645391352263-0b12478ffffe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+          fit: BoxFit.cover),
+    );
+  }
+}
